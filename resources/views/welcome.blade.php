d8ffbb05f5afd697845695be062c15e3980ebc94
ibnuuslbs <ibnuhsaktilubis@gmail.com>
2021-04-15 18:43:08 +0700

    Laravel Framefork

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Laravel
+                Laravel Framefork
             </div>
 
             <div class="links">
